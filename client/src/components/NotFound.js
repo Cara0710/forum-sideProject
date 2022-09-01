193eec20a7fcd48f337709e68235e386c6307377
cara <ha20000710@gmail.com>
2022-09-01 18:16:20 +0800

    add 404 not found page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -3,10 +3,8 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="notfind-page">
-      <div className="notfind">
-        404 Page NotFind <br />
-        你所查詢的頁面可能已經被刪除或不存在
-      </div>
+      <div className="notfind">4&#128531;4 Page NotFound</div>
+      <div className="small"> 你所查詢的頁面可能已經被刪除或不存在</div>
     </div>
   );
 };
