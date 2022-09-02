63bf7b8988bd92f81cf5620f09bc787eb6757de4
cara <ha20000710@gmail.com>
2022-09-02 19:59:20 +0800

    change icon pathname

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,27 +23,27 @@ const Navbar = ({ currentUser, setCurrentUser }) => {
         <div className="left">
           <li>
             <NavLink to="/">
-              <img src="icons/logo-icon.png" alt="logo-icon" />
+              <img src="/icons/logo-icon.png" alt="logo-icon" />
             </NavLink>
           </li>
         </div>
         <div className="right">
           <li>
             <NavLink style={handleNavColor} to="/help">
-              <img src="icons/helper-icon.png" alt="helper-icon" />
+              <img src="/icons/helper-icon.png" alt="helper-icon" />
               <p>使用須知</p>
             </NavLink>
           </li>
           <li>
             <NavLink style={handleNavColor} to="/forum">
-              <img src="icons/forum-icon.png" alt="forun-icon" />
+              <img src="/icons/forum-icon.png" alt="forun-icon" />
               <p>防詐論壇</p>
             </NavLink>
           </li>
           {!currentUser && (
             <li>
               <NavLink style={handleNavColor} to="/register">
-                <img src="icons/register-icon.png" alt="register-icon" />
+                <img src="/icons/register-icon.png" alt="register-icon" />
                 <p>註冊</p>
               </NavLink>
             </li>
@@ -51,7 +51,7 @@ const Navbar = ({ currentUser, setCurrentUser }) => {
           {!currentUser && (
             <li>
               <NavLink style={handleNavColor} to="/login">
-                <img src="icons/login-icon.png" alt="login-icon" />
+                <img src="/icons/login-icon.png" alt="login-icon" />
                 <p>登入</p>
               </NavLink>
             </li>
@@ -59,7 +59,7 @@ const Navbar = ({ currentUser, setCurrentUser }) => {
           {currentUser && (
             <li>
               <NavLink style={handleNavColor} to="/profile">
-                <img src="icons/profile-icon.png" alt="profile-icon" />
+                <img src="/icons/profile-icon.png" alt="profile-icon" />
                 <p>個人檔案</p>
               </NavLink>
             </li>
@@ -67,7 +67,7 @@ const Navbar = ({ currentUser, setCurrentUser }) => {
           {currentUser && (
             <li>
               <NavLink onClick={handleLogout} to="/">
-                <img src="icons/logout-icon.png" alt="logout-icon" />
+                <img src="/icons/logout-icon.png" alt="logout-icon" />
                 <p>登出</p>
               </NavLink>
             </li>
